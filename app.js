bb24a25a57a536214be213b268a4e792302bc4bf
rohithpattathil <62629106+rohithpattathil@users.noreply.github.com>
2020-08-13 16:04:01 -0500

    compose page added and posts show on home

--- app.js
+++ app.js
@@ -3,6 +3,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const posts = [];
 
 const homeStartingContent =
   "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
@@ -19,7 +20,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 app.get("/", function (req, res) {
-  res.render("home", { homeText: homeStartingContent });
+  res.render("home", { homeText: homeStartingContent, newPost: posts });
 });
 
 app.get("/about", function (req, res) {
@@ -35,7 +36,8 @@ app.get("/compose", function (req, res) {
 });
 app.post("/compose", function (req, res) {
   const post = { title: req.body.postTitle, content: req.body.postBody };
-  console.log(post);
+  posts.push(post);
+  res.redirect("/");
 });
 app.listen(3000, function () {
   console.log("Server started on port 3000");
